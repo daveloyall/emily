2961166f8fccccc6fa99ec70954941ee42cca441
hobbes <dave@the-good-guys.net>
2016-07-19 23:57:35 -0500

    embot1 can connect to an ircd and say hello

--- mockup/irc-client.lisp
+++ mockup/irc-client.lisp
@@ -1,5 +1,14 @@
 (ql:quickload :birch)
 
 
-(defclass my-connection (connection) ())
+(defclass my-connection (birch:connection) ())
 
+(defvar *connection* (make-instance 'my-connection
+                                    :server-host "127.0.0.1"
+                                    :nick "embot1"))
+
+
+(birch:connect *connection*)
+
+(birch:/privmsg *connection* "#test" "hello")
+(birch:/quit *connection*)
