8218e3e2a69b43223e243d4654e06e4f386e18b5
Dave Loyall <dave@the-good-guys.net>
2016-09-13 15:44:17 -0500

    changes found laying around

--- mockup/irc-client.lisp
+++ mockup/irc-client.lisp
@@ -1,14 +1,20 @@
 (ql:quickload :birch)
 
+(defpackage :emily (:use :birch :cl))
+*package*
+(in-package :emily)
 
-(defclass my-connection (birch:connection) ())
+(defclass my-connection (connection) ())
 
 (defvar *connection* (make-instance 'my-connection
-                                    :server-host "127.0.0.1"
+                                    :server-host "metalbaby"
                                     :nick "embot1"))
 
+(defmethod handle-event ((connection my-connection) (event privmsg-event))
+  (format t "Message received on ~A: ~A" (channel event) (message event)))
 
-(birch:connect *connection*)
+(connect *connection*)
 
-(birch:/privmsg *connection* "#test" "hello")
-(birch:/quit *connection*)
+(/privmsg *connection* "#test" "helllllo")
+(/quit *connection*)
+(/join *connection* "#test")
